6599b51d2aa8617c51b1e06f187a120cce623208
mashuko <maria10vanilla@gmail.com>
2025-02-25 07:57:59 +0300

    Add files via upload

--- task 5/1_5.cpp
+++ task 5/1_5.cpp
@@ -51,4 +51,48 @@ public:
             delete temp;
         }
     }
+
+    void error() const{
+        if (!head){
+            throw std::runtime_error("Queue is empty Y_Y");
+        }
+    }
+
+    PriorityQueue& enqueue(int priority, const char* data){
+        Node* new_node = new Node(priority, data);
+        if (!head || priority < head->priority){
+            new_node->next = head;
+            head = new_node;
+        }
+        else{
+            Node* current = head;
+            while(current->next && current->next->priority <= priority){
+                current = current->next;
+            }
+            new_node->next = current->next;
+            current->next = new_node;
+        }
+        return *this;
+    }
+
+    const char* peek() const{
+        error();
+        return head->data;
+    }
+
+    PriorityQueue& dequeue(){
+        error();
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return *this;
+    }
+
+    PriorityQueue& merge(PriorityQueue& other){
+        while(other.head){
+            enqueue(other.head->priority, other.head->data);
+            other.dequeue();
+        }
+        return *this;
+    }
 };
